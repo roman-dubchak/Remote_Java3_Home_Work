c994432f23d55c931efbd8aed34284bb09d70242
Roman Dubchak <dubrathers@mail.ru>
2020-10-19 10:13:51 +0300

    upd lesson1 HW Fruits

---
+++ src/Lesson1_generics/Fruit.java
@@ -0,0 +1,13 @@
+package Lesson1_generics;
+
+public abstract class Fruit {
+    private double weight;
+
+    public double getWeight(){
+        return this.weight;
+    }
+
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+}
---
+++ src/Lesson1_generics/Orange.java
@@ -0,0 +1,9 @@
+package Lesson1_generics;
+
+public class Orange extends Fruit{
+    private double weight;
+
+    public Orange(){
+        setWeight(1.5f);
+    }
+}
